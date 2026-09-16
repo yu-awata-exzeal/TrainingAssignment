7efdcb47c790297942bda28aac26a20c7e94434d
yu.awata <yu.awata@exzeal.co.jp>
2026-09-16 13:17:00 +0900

    拠点電力ゲージの時間経過減少とインタラクト増加を追加

--- TrainingAssignment/Assets/Script/ScriptableObject/StageSettings.cs
+++ TrainingAssignment/Assets/Script/ScriptableObject/StageSettings.cs
@@ -3,7 +3,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "StageSettings")]
 public class StageSettings : ScriptableObject
 {
+    /// <summary>
+    /// 拠点のエネルギー量上限
+    /// </summary>
     public int MaxBasePointEnergy;
+    /// <summary>
+    /// プレイヤー携帯のエネルギー量
+    /// </summary>
     public int MaxPlayerEnergy;
+    /// <summary>
+    /// 時間経過のエネルギー消費量
+    /// </summary>
+    public int EnergyConsumptionRate;
+    /// <summary>
+    /// 燃料アイテムのエネルギー回復量
+    /// </summary>
     public int FuelContent;
 }
